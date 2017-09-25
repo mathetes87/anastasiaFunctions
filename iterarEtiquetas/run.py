a5549c3b59761eed07197ed496c0acdc9af3c2af
Juan Roberto Honorato <juanrhonorato@gmail.com>
2017-09-25 13:17:10 -0300

    Added iterar etiquetas (Sodimac)

--- iterarEtiquetas/run.py
+++ iterarEtiquetas/run.py
@@ -1,121 +1,125 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-import os, json, csv, unicodedata
+# coding: utf-8
+import json, csv, requests
 
-try:
-    post_data = json.loads(open(os.environ['req']).read())
-except:
-    post_data = {
-        "dominio": "diabetes",
-        "query": "embarazo",
-        "etiqueta": "definiciones de d.g",
-        "profundidad": 2
-    }
-try:
-    response = open(os.environ['res'], 'w')
-except:
-    response = open('dummy_output.txt', 'w')
+query = 'busco parrilla'
 
-# para quitar acentos
-def remove_accents(input_str):
-    if type(input_str) == str:
-        input_str = unicode(input_str, 'utf-8')
-    nfkd_form = unicodedata.normalize('NFKD', input_str)
-    return u"".join([c for c in nfkd_form if not unicodedata.combining(c)])
+url ='https://language.googleapis.com/v1beta2/documents:analyzeSyntax?fields=language%2Ctokens&key=AIzaSyCeC5Dnx1qOfNKgUY6PUnl8IcCcx53nLwQ'
+params = {
+    "document": 
+        {
+            "content": query,
+            "language": "es",
+            "type": "PLAIN_TEXT"
+        }
+}
 
-# obtener query de llamada sin acentos
-query = remove_accents(post_data['query'].lower())
+r = requests.post(url, data=json.dumps(params))
+tokens = json.loads(r.text)['tokens']
 
-# leer base de etiquetas
-etiquetas_db = json.loads(open('../sharedFiles/etiquetas_db.json', 'rt').read())
-dominio_db = etiquetas_db[post_data['dominio'].lower()]
-
-# encontrar subetiquetas a partir de nivel de profundidad indicado
-label = post_data['etiqueta'].lower()
-depth = post_data['profundidad']
-with open("../sharedFiles/arbol_etiquetas.csv", "rt") as f:
-    reader = csv.reader(f, delimiter=',')
-    caminos = list(reader)
-    caminos = [[nodo.lower() for nodo in camino if nodo != ''] for camino in caminos]
-
-caminos_match_etiquetas = []
-for camino in caminos:
-    try:
-        if label == camino[depth]:
-            caminos_match_etiquetas.append(camino)
-    except:
-        pass
-
-# tiene subetiquetas?
-subetiquetas = []
-for camino in caminos_match_etiquetas:
-    if len(camino)-1 > depth:
-        subetiquetas.append(camino[depth+1].encode('utf-8'))
-
-with open("../sharedFiles/resumen_preguntas_etiquetas.tsv", "rt") as f:
-    reader = csv.reader(f, delimiter='\t')
-    preguntas = list(reader)
-
-def get_preguntas(label, depth):
-    sin_resumen = []
-    preguntas_resumen = []
-    for i, col in enumerate(preguntas[0]):
-        if col == 'Etiqueta '+str(depth+1):
-            depth_index = i
-    for row in preguntas:
-        label_at_depth = row[depth_index].lower()
-        resumen_pregunta = row[2]
-        respuesta = row[1]
-        pregunta = row[0] # IDEALMENTE TENER UN ID!!!
-        if label_at_depth == label:
-            if resumen_pregunta == '':
-                sin_resumen.append(pregunta)
-            else:
-                pregunta_resumen = {
-                    'resumen_pregunta': resumen_pregunta, 
-                    'respuesta': respuesta
-                }
-                if pregunta_resumen not in preguntas_resumen:
-                    preguntas_resumen.append(pregunta_resumen)
-    return (sin_resumen, preguntas_resumen)
-
-# armar output
-# si es nivel final, buscar preguntas resumen
-if len(subetiquetas) == 0:
-    # buscar preguntas
-    sin_resumen, preguntas_resumen = get_preguntas(label, depth)
-    
-    output = {
-                'falta_filtrar': False,
-                'preguntas': {
-                            'con_resumen': preguntas_resumen,
-                            'sin_resumen': sin_resumen
-                        },
-                'etiquetas': label,
-                'profundidad': depth
-            }
-# si NO es nivel final, retornarlos para que usuario filtre
-else:
-    labels = [etiqueta.encode('utf-8') for etiqueta in subetiquetas]
-    output = {
-                'falta_filtrar': True,
-                'etiquetas': labels,
-                'profundidad': depth+1,
-            }
-
-def byteify(input):
-    if isinstance(input, dict):
-        return {byteify(key): byteify(value)
-                for key, value in input.iteritems()}
-    elif isinstance(input, list):
-        return [byteify(element) for element in input]
-    elif isinstance(input, unicode):
-        return input.encode('utf-8')
+def distance_to_token(current_token, target_token, distance=0):
+    new_token = tokens[current_token['dependencyEdge']['headTokenIndex']]
+    if current_token == target_token:
+        # si encontré el token que buscaba
+        return distance
+    elif new_token == current_token:
+        # si llegué a la raiz se vuelve circular
+        return distance + 1
     else:
-        return input
+        distance += 1
+        return distance_to_token(new_token, target_token, distance)
+    
+def find_tokens_by_label_or_tag(label_or_tag, identifier):
+    tokens_found = []
+    for i, token in enumerate(tokens):
+        tag_match = (label_or_tag == "label" and token['dependencyEdge']['label'] == identifier)
+        label_match = (label_or_tag == "tag" and token['partOfSpeech']['tag'] == identifier)
+        if label_match or tag_match:
+            tokens_found.append(token)            
+    return tokens_found
 
-output = json.dumps(byteify(output), ensure_ascii=False)
-print output
+def closest_token(current_token, candidates):
+    closest = {'token': None, 'distance': 2112}
+    for candidate in candidates:
+        this_distance = distance_to_token(current_token, candidate)
+        if this_distance < closest['distance']:
+            closest['distance'] = this_distance
+            closest['token'] = candidate
+    return closest['token'], closest['distance']
 
-response.write(output)
-response.close()
+# guardar el sustantivo mas cercano a la raiz
+root = find_tokens_by_label_or_tag("label", identifier='ROOT')[0]
+closest_verb, _ = closest_token(root, find_tokens_by_label_or_tag("tag", identifier='VERB'))
+closest_noun, _ = closest_token(root, find_tokens_by_label_or_tag("tag", identifier='NOUN'))
+        
+print "Raíz de la oración: '{}'".format(root['text']['content'])
+print "Sustantivo más cercano: '{}'".format(closest_noun['text']['content'])
+
+producto = closest_noun['text']['content']
+
+def unicode_csv_reader(utf8_data, dialect=csv.excel, **kwargs):
+    as_list = []
+    csv_reader = csv.reader(utf8_data, dialect=dialect, **kwargs)
+    for row in csv_reader:
+        as_list.append([unicode(cell, 'utf-8') for cell in row])
+    return as_list
+
+data = unicode_csv_reader(open('../sharedFiles/Web scraping 15k.csv'))
+
+header = data[0]
+data = data[1:]
+
+def n_categorias(first_row, n=0):
+    if ('Categoria '+str(n+1)) in first_row:
+        return n_categorias(first_row, n+1)
+    else:
+        return n
+
+n_categorias = n_categorias(header)
+
+# quitar productos sin match de pasillo
+filtered = [row for row in data if "Sin info" not in row]
+
+# dejar solo donde calza el producto
+filtered = [row for row in filtered if producto in row[header.index('Nombre')]]
+print "Productos encontrados: {} {}".format(len(filtered), producto)
+
+def filtrar_productos(filtered, categoria_inicial=1):
+    print ""
+    pasillos = set([row[header.index('pasillo')] for row in filtered])
+    if len(pasillos) <= 1:
+        return filtered
+    else:
+        # buscar donde bifurcan categorias
+        for i in range(categoria_inicial, n_categorias):
+            index_categoria = header.index('Categoria '+str(i))
+            categorias = set([row[index_categoria] for row in filtered])
+            if len(categorias) > 1:
+                # tengo todavia mas de una categoria
+                print "Filtra las categorías del producto por su número:"
+                for j, categoria in enumerate(categorias):
+                    print j+1, categoria
+                print j+2, "No estoy seguro/Indiferente"
+                print ""
+                eleccion = int(raw_input())
+                # filtrar resultados y chequear de nuevo de forma recursiva
+                if eleccion == j+2:
+                    print "Indiferente", categoria_inicial
+                    return filtrar_productos(filtered, categoria_inicial=categoria_inicial+1)
+                else:
+                    filtered = [row for row in filtered if list(categorias)[eleccion-1] == row[index_categoria]]
+                    return filtrar_productos(filtered)
+            else:
+                continue
+        # si salgo es que no puedo seguir filtrando por categorias y quedan pasillos distintos
+        print "No se puede seguir filtrando por categorias"
+        return filtered
+
+output = filtrar_productos(filtered)
+
+if output:
+    pasillos = list(set([row[header.index('pasillo')] for row in output]))
+    if len(pasillos) > 1:
+        print "Tenemos productos en distintos pasillos: {}".format(','.join(pasillos))
+        
+    else:
+        print "Ubicación del producto: {}".format(pasillos[0])
---
+++ iterarProductos/run.py
@@ -0,0 +1,155 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import os, json, csv, unicodedata
+
+try:
+    post_data = json.loads(open(os.environ['req']).read())
+except:
+    post_data = {
+        "dominio": "diabetes",
+        "query": "población"
+    }
+try:
+    response = open(os.environ['res'], 'w')
+except:
+    response = open('dummy_output.txt', 'w')
+
+# para quitar acentos
+def remove_accents(input_str):
+    if type(input_str) == str:
+        input_str = unicode(input_str, 'utf-8')
+    nfkd_form = unicodedata.normalize('NFKD', input_str)
+    return u"".join([c for c in nfkd_form if not unicodedata.combining(c)])
+
+# para obtener listado de sinonimos
+with open('../sharedFiles/Sinonimos_w2v.csv', 'rt') as f:
+    reader = csv.reader(f, delimiter=',')
+    sinonimos = list(reader)
+def get_sinonimos(keyword):
+    for group in sinonimos:
+        if keyword in group:
+            return group
+    return [keyword]
+
+# obtener query de llamada sin acentos
+query = remove_accents(post_data['query'].lower())
+
+# leer base de etiquetas
+etiquetas_db = json.loads(open('../sharedFiles/etiquetas_db.json', 'rt').read())
+dominio_db = etiquetas_db[post_data['dominio']]
+
+# buscar etiquetas en query
+# no repetirlas, quitar acentos y buscar sinonimos
+labels = []
+for label, keyword in dominio_db:
+    label = label.capitalize()
+    keywords_syn = get_sinonimos(keyword)
+    # buscar keyword y sus sinonimos en base de etiquetas
+    for keyword_syn in keywords_syn:
+        if remove_accents(keyword_syn) in query and len(keyword_syn) > 1:
+            # revisar que etiqueta no este repetida
+            if label not in labels:
+                labels.append(label)
+                
+# identificar etiquetas mas profundas
+# primero identificar caminos en el arbol, quitando nodos vacios
+caminos_match_etiquetas = []
+with open("../sharedFiles/arbol_etiquetas.csv", "rt") as f:
+    reader = csv.reader(f, delimiter=',')
+    caminos = list(reader)
+    caminos = [[nodo for nodo in camino if nodo != ''] for camino in caminos]
+
+# armar listado auxiliar con match entre etiquetas y caminos
+# registrar profundidad de nodo identificado
+max_label_depth = -1
+for label in labels:
+    for camino in caminos:
+        if label in camino:
+            for idx, node in enumerate(camino):
+                if label == node and idx >= max_label_depth:
+                    caminos_match_etiquetas.append([camino, label, idx])
+                    if max_label_depth < idx:
+                        max_label_depth = idx
+
+# dejar solo los nodos mas profundos
+caminos_profundos = []
+for i, camino_match_etiqueta in enumerate(caminos_match_etiquetas):
+    if camino_match_etiqueta[2] == max_label_depth:
+        caminos_profundos.append(camino_match_etiqueta)
+        
+with open("../sharedFiles/resumen_preguntas_etiquetas.tsv", "rt") as f:
+    reader = csv.reader(f, delimiter='\t')
+    preguntas = list(reader)
+
+def get_preguntas(label, depth):
+    sin_resumen = []
+    preguntas_resumen = []
+    for i, col in enumerate(preguntas[0]):
+        if col == 'Etiqueta '+str(depth+1):
+            depth_index = i
+    for row in preguntas:
+        label_at_depth = row[depth_index].lower()
+        resumen_pregunta = row[2]
+        respuesta = row[1]
+        pregunta = row[0] # IDEALMENTE TENER UN ID!!!
+        if label_at_depth == label:
+            if resumen_pregunta == '':
+                sin_resumen.append(pregunta)
+            else:
+                pregunta_resumen = {
+                    'resumen_pregunta': resumen_pregunta, 
+                    'respuesta': respuesta
+                }
+                if pregunta_resumen not in preguntas_resumen:
+                    preguntas_resumen.append(pregunta_resumen)
+    return (sin_resumen, preguntas_resumen)
+
+# si es más de uno, retornarlos para que usuario filtre
+if len(caminos_profundos) > 1:
+    falta_filtrar = True
+    labels = []
+    for camino_profundo in caminos_profundos:
+        # si no tiene subetiquetas, el mismo es la etiqueta
+        try:
+            labels.append({
+                        'etiqueta': camino_profundo[0][max_label_depth+1],
+                        'subetiquetas': True
+                    })
+        except:
+            etiqueta = camino_profundo[0][max_label_depth].lower()
+            sin_resumen, preguntas_resumen = get_preguntas(etiqueta, max_label_depth)
+            labels.append({
+                        'etiqueta': etiqueta,
+                        'subetiquetas': False , 
+                        'preguntas': {
+                                    'con_resumen': preguntas_resumen,
+                                    'sin_resumen': sin_resumen
+                                }, 
+                    })
+else:
+    falta_filtrar = False
+    labels = [row[1].encode('utf-8') for row in caminos_profundos]
+
+depth = max_label_depth
+output = {
+            'etiquetas': labels,
+            'profundidad': depth,
+            'falta_filtrar': falta_filtrar
+        }
+
+def byteify(input):
+    if isinstance(input, dict):
+        return {byteify(key): byteify(value)
+                for key, value in input.iteritems()}
+    elif isinstance(input, list):
+        return [byteify(element) for element in input]
+    elif isinstance(input, unicode):
+        return input.encode('utf-8')
+    else:
+        return input
+
+output = json.dumps(byteify(output), ensure_ascii=False)
+
+print output
+response.write(output)
+response.close()
