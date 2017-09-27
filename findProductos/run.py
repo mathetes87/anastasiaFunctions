b2c36e41840aa6598c717cb700915149b10df83c
Juan Roberto Honorato <juanrhonorato@gmail.com>
2017-09-27 17:30:13 -0300

    Filtro ocupando keywords

--- findProductos/run.py
+++ findProductos/run.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python
 # coding: utf-8
-import json, csv, os
+import json, csv, os, re
 
 # inputs y outputs
 try:
     import requests
-    query = "donde puedo encontrar una parrilla a carbon para exteriores"
+    from tabulate import tabulate
+    query = "donde puedo encontrar colchones"
 
     url ='https://language.googleapis.com/v1beta2/documents:analyzeSyntax?fields=language%2Ctokens&key=AIzaSyCeC5Dnx1qOfNKgUY6PUnl8IcCcx53nLwQ'
     params = {
@@ -75,6 +76,18 @@ def byteify(input):
     else:
         return input
 
+def filter_data_by_word(word, column_index, data, case_sensitive=True, keep_if_found=True):
+    if case_sensitive:
+        data_lower = [[col.lower() for col in row] for row in data]
+        word = word.lower()
+    else:
+        data_lower = data
+    if keep_if_found:
+        #print row
+        return [row for row in data_lower if word in row[column_index]]
+    else:
+        return [row for row in data_lower if word not in row[column_index]]
+
 def search_pattern(pattern, label_or_tag):
     if label_or_tag == "tag":
         if ",".join(pattern) in ",".join(tags_list):
@@ -99,6 +112,18 @@ def search_pattern(pattern, label_or_tag):
     else:
         pass
 
+def unicode_csv_reader(utf8_data, dialect=csv.excel, **kwargs):
+    as_list = []
+    csv_reader = csv.reader(utf8_data, dialect=dialect, **kwargs)
+    for row in csv_reader:
+        as_list.append([unicode(cell, 'utf-8') for cell in row])
+    return as_list
+
+def n_categorias(first_row, n=0):
+    if ('Categoria '+str(n+1)) in first_row:
+        return n_categorias(first_row, n+1)
+    else:
+        return n
 
 root = find_tokens_by_label_or_tag("label", identifier='ROOT')[0]
 closest_noun, _ = closest_token(root, find_tokens_by_label_or_tag("tag", identifier='NOUN'))
@@ -113,9 +138,9 @@ second_noun = pattern_indexes[2] if pattern_indexes else None
 #--------------------------------------------------------------------------------
 keywords = []
 if root_is_noun:
-    producto = root['text']['content']
+    producto = root
 else:
-    producto = closest_noun['text']['content']
+    producto = closest_noun
 
 if first_noun and tokens[first_noun] is not closest_noun:
     keywords.append(tokens[first_noun]['text']['content']) 
@@ -128,47 +153,63 @@ if second_noun:
     if third_noun:
         keywords.append(tokens[third_noun]['text']['content'])
 
-print "Producto buscado: '{}'".format(repr(closest_noun['text']['content']))
+try:
+    print "Producto buscado: '{}' ({})".format(repr(closest_noun['text']['content']), repr(closest_noun['lemma']))
+except:
+    pass
 print "Palabras clave adicionales de la búsqueda: {}".format(repr(",".join(keywords)))
 
-# leer base de productos
-def unicode_csv_reader(utf8_data, dialect=csv.excel, **kwargs):
-    as_list = []
-    csv_reader = csv.reader(utf8_data, dialect=dialect, **kwargs)
-    for row in csv_reader:
-        as_list.append([unicode(cell, 'utf-8') for cell in row])
-    return as_list
-
+#--------------------------------------------------------------------------------
+# Leer y filtrar base de productos
+#--------------------------------------------------------------------------------
 data = unicode_csv_reader(open('../sharedFiles/Web scraping 15k.csv'))
 
 header = data[0]
 data = data[1:]
 
-def n_categorias(first_row, n=0):
-    if ('Categoria '+str(n+1)) in first_row:
-        return n_categorias(first_row, n+1)
-    else:
-        return n
+def print_sans(data):
+    pop = [6,7, 8, 9, 11, 12]
+    data_sans = [[col for i,col in enumerate(row) if i not in pop] for row in data]
+    header_sans = [col for i,col in enumerate(header) if i not in pop]
+    try:
+        print tabulate(data_sans, headers=header_sans)
+    except:
+        pass
 
 n_categorias = n_categorias(header)
 
 # quitar productos sin match de pasillo
-raw_data = [row for row in data if "Sin info" not in row[-1]]
+filtered_base = filter_data_by_word("Sin info", -1, data, case_sensitive=False, keep_if_found=False)
 
-# dejar solo donde calza el producto
-filtered = [row for row in raw_data if producto in row[header.index('Nombre')].lower()]
+# filtrar segun nombre del producto
+filtered = filter_data_by_word(producto['text']['content'], header.index('Nombre'), filtered_base)
 
-# si no encontré nada, intentar con el lemma
-if len(filtered) == 0:
-    try:
-        closest_noun_lemma = closest_noun['lemma']
-        filtered = [row for row in raw_data if closest_noun_lemma in row[header.index('Nombre')].lower()]
-    except:
-        pass
+try:
+    # unir filtrado de lemma sin duplicar filas
+    aux_filtered_lemma = filter_data_by_word(producto['lemma'], header.index('Nombre'), filtered_base)
+    for row in aux_filtered_lemma:
+        if row not in filtered:
+            filtered.append(row)
+except:
+    pass
+
+# filtrar utilizando las palabras clave encontradas
+have_keyword = []
+for keyword in keywords:
+    for i in range(n_categorias):
+        have_keyword.extend(filter_data_by_word(keyword, header.index('Categoria '+str(i+1)), filtered, case_sensitive=False))
+    have_keyword.extend(filter_data_by_word(keyword, header.index('Nombre'), filtered, case_sensitive=False))
+
+# si quedaron filas tras filtrar por keywords, ocupar esos datos
+if have_keyword:
+    filtered = []
+    for row in have_keyword:
+        if row not in filtered:
+            filtered.append(row)
 
 # llenar categorias que estan vacias con el mas cercano hacia izquierda
 for row in filtered:
-    if row[n_categorias-1] == u'':
+    if row[header.index('Categoria '+str(n_categorias-1))] == u'':
         # buscar primera columna no vacia desde derecha
         for i in range(n_categorias-1, -1, -1):
             if row[i] != '':
@@ -177,7 +218,7 @@ for row in filtered:
                     row[j] = row[i]
                 break
 
-print "\nProductos encontrados: {} {}".format(len(filtered), repr(producto))
+print "\nProductos encontrados: {} {}".format(len(filtered), repr(producto['text']['content']))
 
 if filtered:
     pasillos = list(set([row[header.index('pasillo')] for row in filtered]))
@@ -199,7 +240,10 @@ for i in range(1, n_categorias):
         categoria_actual = i
         break
 
-categorias_a_usuario.extend(["No sé/Indiferente"])
+if not seguir_filtrando:
+    categorias_a_usuario = []
+else:
+   categorias_a_usuario.extend(["Subcategorías"])
 
 output = {
     'header': header,
@@ -212,9 +256,9 @@ output = {
 
 output = json.dumps(byteify(output), ensure_ascii=False)
 
-print byteify(output)
+print_sans(filtered)
 
 print ""
-print output
+#print output
 response.write(output)
 response.close()
